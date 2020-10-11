948d0bd5c9f5976b9b9448bbc847e19f41d43507
Informatica <giuliani.marco@gmail.com>
2020-10-11 12:24:36 +0200

    Creato il metodo comunica nella classe server

--- EsCalcolatrice/ServerCalc/src/Server.java
+++ EsCalcolatrice/ServerCalc/src/Server.java
@@ -7,7 +7,7 @@ public class Server {
     String stringaRicevuta;
     String stringaModificata;
     BufferedReader inDalCliente;
-    DataOutputStream outVersoServer;
+    DataOutputStream outVersoClient;
     
     public Socket attendi()
     {
@@ -15,10 +15,36 @@ public class Server {
             System.out.println("Server avviato");
             server = new ServerSocket(6789);
             client = server.accept();
+            server.close();
+            inDalCliente = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            outVersoClient = new DataOutputStream(client.getOutputStream());
         }
         catch(Exception e)
         {
-            
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server");
+            System.exit(1);
+        }
+        return client;
+    }
+    
+    public void comunica()
+    {
+        try{
+            System.out.println("Benvenuto");
+            stringaRicevuta = inDalCliente.readLine();
+            System.out.println("Ricevuto");
+            stringaModificata = stringaRicevuta.toUpperCase();
+            System.out.println("Invio risposta");
+            outVersoClient.writeBytes(stringaModificata+"\n");
+            System.out.println("Fine");
+            client.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la comunicazione col client");
+            System.exit(1);
         }
     }
 }
